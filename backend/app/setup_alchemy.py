9a5ed033e71e113a34f10e7d7c5a193d257eb3d5
Mark Schuit <mark@gig.solutions>
2024-07-12 09:08:14 +0200

    [IMP] backend - Use environment variables instead of hardcoded connection parameters.

--- backend/app/setup_alchemy.py
+++ backend/app/setup_alchemy.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine, URL
 
 from .orm_models import Base
@@ -7,11 +9,11 @@ def setup_sql_alchemy():
     # Postgres / SqlAlchemy setup
     url = URL.create(
         drivername="postgresql",
-        username="backend",
-        password="hello",
-        host="db",
-        port=5432,
-        database="sensors"
+        username=os.environ.get("PGUSER"),
+        password=os.environ.get("PGPASSWORD"),
+        host=os.environ.get("PGPHOST"),
+        port=os.environ.get("PGPORT"),
+        database=os.environ.get("PGPDATABASE")
     )
     engine = create_engine(url)
     engine.connect()
